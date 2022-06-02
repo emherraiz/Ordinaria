7dd55c6e2e6c4886cd27f77421fb19071d529113
Enrique Marina <eherraiz@myuax.com>
2022-06-02 12:33:22 +0200

    s

--- Ejercicio_POO/tipos_de_tweets.py
+++ Ejercicio_POO/tipos_de_tweets.py
@@ -13,17 +13,27 @@ class Tweet:
     def get_sender(self):
         return self.sender
 
+    def __str__(self):
+        return f'Time: {self.time}\nMessage: {self.message}\nSender: {self.sender}'
+
 
 class Retweet(Tweet):
-    def __init__(self, referencia):
-        super().__init__()
+    def __init__(self, time, message, sender, referencia):
+        super().__init__(time, message, sender)
         self.referente = referencia
+
+    def __str__(self):
+        return f'Time: {self.time}\nMessage: {self.message}\nSender: {self.sender}\nReferente: {self.referente}'
 
 
 class DirectMessage(Tweet):
-    def __init__(self, receiver):
-        super().__init__()
+    def __init__(self, time, message, sender, receiver):
+        super().__init__(time, message, sender)
         self.receiver = receiver
+
+    def __str__(self):
+        return f'Time: {self.time}\nMessage: {self.message}\nSender: {self.sender}\nReceiver: {self.receiver}'
+
 
 
 
